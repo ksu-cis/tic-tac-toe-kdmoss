213d8b64c3c84d9c8ed149dce571fb67ad594589
kdmoss <kdmossbu@gmail.com>
2019-11-11 18:15:05 -0600

    Finished writing javascript logic, css, and html.

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,83 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+let player = "X";
+
+function setTurn() {
+
+    var turnE = document.getElementById("turn");
+    turnE.innerText = `It is ${player}'s turn.`;;
+}
+
+function setWinner(player) {
+
+    var turnE = document.getElementById("turn");
+    turnE.innerText = `${player} wins!`;
+}
+
+function onClick(event) {
+
+    event.preventDefault();
+
+    if (!event.target.innerText) {
+
+        event.target.innerText = player;
+
+        if (player === "X")
+            player = "O";
+        else
+            player = "X";
+        
+        if (!isOver())
+            setTurn();
+    }
+}
+
+function isOver() {
+
+    let cells = document.getElementsByClassName("square");
+
+    for (let i = 0; i < 9; i += 3) {
+
+        if (cells[i].innerText !== "" && 
+		cells[i].innerText === cells[i + 1].innerText && 
+		cells[i + 1].innerText === cells[i + 2].innerText) {
+
+            setWinner(cells[i].innerText);
+            return true;
+        }
+    }
+
+    for (let i = 0; i < 3; i++) {
+
+        if (cells[i].innerText !== "" && 
+		cells[i].innerText === cells[i + 3].innerText && 
+		cells[i + 3].innerText === cells[i + 6].innerText) {
+
+            setWinner(cells[i].innerText);
+            return true;
+        }
+    }
+
+    if (cells[0].innerText !== "" && cells[0].innerText === cells[4].innerText &&
+	cells[4].innerText === cells[8].innerText) {
+
+        setWinner(cells[4].innerText);
+        return true;
+    }
+
+    if (cells[2].innerText !== "" && cells[2].innerText === cells[4].innerText && 
+	cells[4].innerText === cells[6].innerText) {
+
+        setWinner(cells[4].innerText);
+        return true;
+    }
+    
+}
+
+let cells = document.getElementsByClassName("square");
+
+for (i = 0; i < cells.length; i++) 
+    cells[i].addEventListener("click", onClick);
+
+setTurn();
